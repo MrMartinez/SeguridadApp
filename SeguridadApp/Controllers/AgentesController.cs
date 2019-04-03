8bb8851c0c2de94f197f0f6d58729004eb888236
Welinton Martinez <wmartinez@hdssd.semma.gob.do>
2019-04-02 23:37:04 -0400

    Terminada relacion con tabla Rangos

--- SeguridadApp/Controllers/AgentesController.cs
+++ SeguridadApp/Controllers/AgentesController.cs
@@ -1,4 +1,5 @@
 ﻿using SeguridadApp.Models;
+using SeguridadApp.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -16,7 +17,7 @@ namespace SeguridadApp.Controllers
         string connectionString = "Data Source = .; Initial Catalog = SeguridadDpto; Integrated Security = true";
         CultureInfo culture = new CultureInfo("en-US"); //Para manejar el formato de la fecha cuando la parseo
 
-     
+
         // GET: Agentes
         public ActionResult Index()
         {
@@ -26,20 +27,33 @@ namespace SeguridadApp.Controllers
 
 
             conn.Open();
-            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM AGENTES", conn);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM AGENTES JOIN RANGOS on Agentes.RangoId = Rangos.Id", conn);
             da.Fill(dt);
 
-            List<Agente> agente = dt.Rows.OfType<DataRow>().Select(x => new Agente()
+            List<AgenteRangoViewModel> agente = dt.Rows.OfType<DataRow>().Select(x => new AgenteRangoViewModel()
             {
-                Id = Convert.ToInt16(x[0].ToString()),
+                //AgenteId = Convert.ToInt16(x[0].ToString()),
+                //Apellido1 = x[1].ToString(),
+                //Apellido2 = x[2].ToString(),
+                //Nombres = x[3].ToString(),
+                //Cedula = Convert.ToInt64(x[4].ToString()),
+                //Descripcion =int.Parse(x[5].ToString()),
+                //FechaNacimiento = DateTime.Parse(x[6].ToString(), culture),
+                //Telefono = Convert.ToInt64(x[7].ToString()),
+                //Foto = "/Fotos/" + x[3].ToString() + ".jpg",
+                AgenteId = Convert.ToInt16(x[0].ToString()),
+                RangoId = Convert.ToInt16(x[5].ToString()),
                 Apellido1 = x[1].ToString(),
                 Apellido2 = x[2].ToString(),
                 Nombres = x[3].ToString(),
                 Cedula = Convert.ToInt64(x[4].ToString()),
-                Rango = x[5].ToString(),
-                FechaNacimiento = DateTime.Parse(x[6].ToString(), culture),
+                DescripcionRango = x[10].ToString(),
                 Telefono = Convert.ToInt64(x[7].ToString()),
-                Foto = "/Fotos/" + x[3].ToString() + ".jpg"
+                Foto = "/Fotos/" + x[3].ToString() + ".jpg",
+
+
+
+
             }).ToList();
 
             #region Codigo anterior con el que asignaba valores a la clase desde la ROW; no me funcino para pasar la row como modelo
@@ -60,19 +74,32 @@ namespace SeguridadApp.Controllers
 
         public ActionResult Create()
         {
+            DataTable dt = new DataTable();
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM RANGOS", conn);
+            da.Fill(dt);
 
+            List<Rango> Rangos = dt.Rows.OfType<DataRow>().Select(x => new Rango()
+            {
+                Id = int.Parse(x[0].ToString()),
+                Descripcion = x[1].ToString()
+
+            }).ToList();
+            ViewBag.RangoId = new SelectList(Rangos, "Id", "Descripcion");
             return View();
         }
 
         [HttpPost]
         public ActionResult Create(FormCollection form)
         {
+
             var newFoto = form["uploadFoto"].ToString();
             var newApellido1 = form["Apellido1"];
             var newApellido2 = form["Apellido2"];
             var newNombres = form["nombres"];
             var newCedula = Convert.ToInt64(form["cedula"]);
-            var newRango = form["rango"];
+            var newRangoId = form["RangoId"];
             var newFechaNacimiento = DateTime.Parse(form["fechaNacimiento"], culture);
             var newTelefono = Convert.ToInt64(form["telefono"]);
             using (SqlConnection conn = new SqlConnection(connectionString))
@@ -85,12 +112,12 @@ namespace SeguridadApp.Controllers
                 cmd.Parameters.AddWithValue("@Apellido2", newApellido2);
                 cmd.Parameters.AddWithValue("@Nombres", newNombres);
                 cmd.Parameters.AddWithValue("@Cedula", newCedula);
-                cmd.Parameters.AddWithValue("@Rango", newRango);
+                cmd.Parameters.AddWithValue("@Rango", newRangoId);
                 cmd.Parameters.AddWithValue("@FechaNacimiento", newFechaNacimiento);
                 cmd.Parameters.AddWithValue("@Telefono", newTelefono);
                 cmd.Parameters.AddWithValue("@Foto", newFoto);
 
-                cmd.CommandText = @"INSERT INTO Agentes (Apellido1, Apellido2, Nombres, Cedula, Rango, FechaNacimiento, Telefono, Foto ) 
+                cmd.CommandText = @"INSERT INTO Agentes (Apellido1, Apellido2, Nombres, Cedula, RangoId, FechaNacimiento, Telefono, Foto ) 
                                                 VALUES(@Apellido1, @Apellido2, @Nombres, @Cedula, @Rango, @FechaNacimiento, @Telefono,@Foto) ";
 
                 cmd.ExecuteNonQuery();
@@ -109,7 +136,7 @@ namespace SeguridadApp.Controllers
             conn.Open();
             SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM AGENTES WHERE Id = " + id + "", conn);
             da.Fill(dt);
-       
+
 
             Agente agente = new Agente();
             agente.Id = id;
@@ -117,11 +144,24 @@ namespace SeguridadApp.Controllers
             agente.Apellido2 = dt.Rows[0][2].ToString();
             agente.Nombres = dt.Rows[0][3].ToString();
             agente.Cedula = Convert.ToInt64(dt.Rows[0][4].ToString());
-            agente.Rango = dt.Rows[0][5].ToString();
+            agente.RangoId = int.Parse(dt.Rows[0][5].ToString());
             agente.FechaNacimiento = DateTime.Parse(dt.Rows[0][6].ToString());
             agente.Telefono = Convert.ToInt64(dt.Rows[0][7].ToString());
-            agente.Foto = "/Fotos/" + dt.Rows[0][3].ToString() + ".jpg"; 
-       
+            agente.Foto = "/Fotos/" + dt.Rows[0][3].ToString() + ".jpg";
+
+
+            DataTable dtRangos = new DataTable();
+           
+            SqlDataAdapter daRangos = new SqlDataAdapter("SELECT * FROM RANGOS", conn);
+            daRangos.Fill(dtRangos);
+
+            List<Rango> Rangos = dtRangos.Rows.OfType<DataRow>().Select(x => new Rango()
+            {
+                Id = int.Parse(x[0].ToString()),
+                Descripcion = x[1].ToString()
+
+            }).ToList();
+            ViewBag.RangoId = new SelectList(Rangos, "Id", "Descripcion");
             return View(agente);
         }
         [HttpPost]
@@ -133,7 +173,7 @@ namespace SeguridadApp.Controllers
             var newApellido2 = form["Apellido2"];
             var newNombres = form["nombres"];
             var newCedula = Convert.ToInt64(form["cedula"]);
-            var newRango = form["rango"];
+            var newRango = int.Parse(form["RangoId"]);
             var newFechaNacimiento = DateTime.Parse(form["fechaNacimiento"], culture);
             var newTelefono = Convert.ToInt64(form["telefono"]);
             using (SqlConnection conn = new SqlConnection(connectionString))
@@ -152,8 +192,8 @@ namespace SeguridadApp.Controllers
                 cmd.Parameters.AddWithValue("@Foto", newFoto);
 
                 cmd.CommandText = @"UPDATE Agentes SET Apellido1 = @Apellido1, Apellido2 = @Apellido2, Nombres = @Nombres, Cedula = @Cedula,
-                                                                    Rango = @Rango, FechaNacimiento = @FechaNacimiento, Telefono =@Telefono,
-                                                                    Foto = @Foto WHERE ID = '"+id+"'";
+                                                                    RangoId = @Rango, FechaNacimiento = @FechaNacimiento, Telefono =@Telefono,
+                                                                    Foto = @Foto WHERE ID = '" + id + "'";
 
                 cmd.ExecuteNonQuery();
                 conn.Close();
@@ -171,19 +211,21 @@ namespace SeguridadApp.Controllers
 
 
             conn.Open();
-            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM AGENTES WHERE Id = "+id+"", conn);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM AGENTES a JOIN RANGOS r on a.RangoId = r.Id WHERE a.Id = " + id + "", conn);
             da.Fill(dt);
 
-            Agente agente = new Agente();
+            AgenteRangoViewModel agente = new AgenteRangoViewModel();
             {
+                agente.AgenteId = int.Parse(dt.Rows[0][0].ToString());
                 agente.Apellido1 = dt.Rows[0][1].ToString();
                 agente.Apellido2 = dt.Rows[0][2].ToString();
                 agente.Nombres = dt.Rows[0][3].ToString();
                 agente.Cedula = Convert.ToInt64(dt.Rows[0][4].ToString());
-                agente.Rango = dt.Rows[0][5].ToString();
+                agente.RangoId = int.Parse(dt.Rows[0][5].ToString());
                 agente.FechaNacimiento = DateTime.Parse(dt.Rows[0][6].ToString(), culture);
                 agente.Telefono = Convert.ToInt64(dt.Rows[0][7].ToString());
                 agente.Foto = "/Fotos/" + dt.Rows[0][3].ToString() + ".jpg";
+                agente.DescripcionRango = dt.Rows[0][10].ToString();
             };
             return View(agente);
         }
--- SeguridadApp/ViewModels/AgenteRangoViewModel.cs
+++ SeguridadApp/ViewModels/AgenteRangoViewModel.cs
@@ -4,22 +4,22 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace SeguridadApp.Models
+namespace SeguridadApp.ViewModels
 {
-    public class Agente
+    public class AgenteRangoViewModel
     {
-        public int Id { get; set; }
+        public int AgenteId { get; set; }
+        public int RangoId { get; set; }
+        public int MyProperty { get; set; }
         public string Apellido1 { get; set; }
         public string Apellido2 { get; set; }
         public string Nombres { get; set; }
         public long Cedula { get; set; }
+        public long Telefono { get; set; }
+        public string Foto { get; set; }
+        public string DescripcionRango { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime FechaNacimiento { get; set; }
-        public long Telefono { get; set; }
-        public string Foto { get; set; }
-        public int RangoId { get; set; }
-        public Rango Rango { get; set; }
-
     }
 }
